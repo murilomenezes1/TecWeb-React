7771a27228ad5d4e8ef488671a560a8af2111a30
murilo menezes <murilolcm@al.insper.edu.br>
2020-11-11 15:17:22 -0300

    heroku deploy fix

--- stockmern/src/signup.jsx
+++ stockmern/src/signup.jsx
@@ -55,7 +55,7 @@ export default class Signup extends Component{
 	}
 
 		signup(){
-			axios.post('http://localhost:3000/signup', this.state.user).then(resp =>{
+			axios.post('https://stockmern-react.herokuapp.com/signup', this.state.user).then(resp =>{
 				if(Math.floor(resp.status/100)===2){
 					this.setState((state) =>{
 						return {
