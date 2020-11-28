f875541f482cae824fd83bc8faaf698bde8f85f7
Enrico Damiani <enrico.francesco.damiani2gmail.com>
2020-11-28 17:48:10 -0300

    teste marketCap

--- src/tests/Stocks.test.js
+++ src/tests/Stocks.test.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
 
-import { fireEvent, cleanup } from '@testing-library/react';
-import { waitFor } from '@testing-library/dom';
+import { fireEvent, cleanup } from "@testing-library/react";
+import { waitFor } from "@testing-library/dom";
 
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
 
-import { mount } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import Stocks from "../views/Stocks/Stocks";
+import axios from "axios";
 
-import Stocks from '../views/Stocks/Stocks';
-import axios from 'axios';
+jest.mock("axios", () => ({
+  create: jest.fn().mockResolvedValue({ data: {} }),
+  // Criando mock do método GET para listagem de usuários. O objeto
+  //dentro do método mockResolvedValue será o “response”
 
+  //var bestStocks = response.data.finance.result[0].quotes
 
-jest.mock('axios', () => ({
-
-    create: jest.fn().mockResolvedValue({ data: {} }),
-    // Criando mock do método GET para listagem de usuários. O objeto
-    //dentro do método mockResolvedValue será o “response”
-
-    //var bestStocks = response.data.finance.result[0].quotes
-
-    request: jest.fn().mockResolvedValue({
-        data: {
-            finance: {
-                result: [
-                    {
-                        quotes: [
-                            {
-                                symbol: "symbol",
-                                longName: "longName",
-                                regularMarketPrice: "regular"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }),
-    // Criando mock do método POST para cadastro de usuário
-    //post: jest.fn().mockResolvedValue({ data: {} })
-}))
+  request: jest.fn().mockResolvedValue({
+    data: {
+      finance: {
+        result: [
+          {
+            quotes: [
+              {
+                symbol: "symbol",
+                longName: "longName",
+                regularMarketPrice: "regular",
+                marketCap: "market",
+              },
+            ],
+          },
+        ],
+      },
+    },
+  }),
+  // Criando mock do método POST para cadastro de usuário
+  //post: jest.fn().mockResolvedValue({ data: {} })
+}));
 
 // Reseta o render do component após cada teste
-afterEach(cleanup)
+afterEach(cleanup);
 
-it('nome do teste', async () => {
-    const { getByTestId, getByText } = render(<Stocks />);
-    // ...
-    // Confere resultado. Deve esperar a “resposta” do mock antes de
-    //verificar qualquer atualização na tela
-    await waitFor(() => expect(axios.request).toHaveBeenCalledTimes(1))
-    const teste1 = getByText(/symbol/i);
-    expect(teste1).toBeInTheDocument();
+it("nome do teste", async () => {
+  const { getByTestId, getByText } = render(<Stocks />);
+  // ...
+  // Confere resultado. Deve esperar a “resposta” do mock antes de
+  //verificar qualquer atualização na tela
+  await waitFor(() => expect(axios.request).toHaveBeenCalledTimes(1));
+  const teste1 = getByText(/symbol/i);
+  expect(teste1).toBeInTheDocument();
 
-    const teste2 = getByText(/longName/i);
-    expect(teste2).toBeInTheDocument();
+  const teste2 = getByText(/longName/i);
+  expect(teste2).toBeInTheDocument();
 
-    const teste3 = getByText(/regular/i);
-    expect(teste3).toBeInTheDocument();
+  const teste3 = getByText(/regular/i);
+  expect(teste3).toBeInTheDocument();
+
+  const teste4 = getByText(/market/i);
+  expect(teste4).toBeInTheDocument();
 });
-
 
 // it("should render text", () => {
 //     const page = mount(<Stocks/>)
@@ -89,14 +89,12 @@ it('nome do teste', async () => {
 //     expect(trending).toBeInTheDocument();
 // })
 
-
 // test("should have info Price/Earnings of the stock", () => {
 //     render(<Stocks />)
 
 //     const trending = screen.getByText("Forward P/E:");
 //     expect(trending).toBeInTheDocument();
 // })
-
 
 // test("should have info profit margin of the stock", () => {
 //     render(<Stocks />)
@@ -105,7 +103,6 @@ it('nome do teste', async () => {
 //     expect(trending).toBeInTheDocument();
 // })
 
-
 // test("should have info float shares of the stock", () => {
 //     render(<Stocks />)
 
@@ -113,14 +110,12 @@ it('nome do teste', async () => {
 //     expect(fltShares).toBeInTheDocument();
 // })
 
-
 // test("should have info employees of the stock", () => {
 //     render(<Stocks />)
 
 //     const employees = screen.getByText("Funcionários:");
 //     expect(employees).toBeInTheDocument();
 // })
-
 
 // test("should have info country of the stock", () => {
 //     render(<Stocks />)
